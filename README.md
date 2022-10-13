2663026c316c1e670e367316403a5c0444f1ce0f
nkwtnb <n.watanabe0305@gmail.com>
2022-10-13 18:00:27 +0900

    update README

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 after passing to file path and json you want to input to the Excel,  
 make Excel and return it base64 encoded.
 
-## how to request
+## Request
 ### URL
 [URL]/api/v1/form
 
 ### method
 POST
 
-### request header
+### header
 ```
 {
    "Content-Type": "application/json",
@@ -18,7 +18,7 @@ POST
 }
 ```
 
-### request body
+### body
 ```
 {
   "formId": "1",
@@ -63,34 +63,17 @@ POST
 }
 ```
 
-### response
-#### success
+## Response
+### success
 ```
 {
    base64: "base64 encoded data"
 }
 ```
 
-#### error
+### error
 ```
 {
    message: "error message"
 }
 ```
- 
-# Authentication using JWT in RoR
-
-1. `bundle install` (--path vendor/bundle)
-2. add following gems to Gemfile
-   - "bcrypt"
-   - "jwt"
-3. make model
-   1. `bundle exec rails g model user name email password_digest`
-   2. `bundle exec rails db:migrate`
-4. add `has_secure_password` to User model
-5. make secret key
-   1. `mkdir PATH_TO_PROJECT/auth && $_`
-   2. `openssl genrsa 2024 > service.key`
-   3. add service.key to .gitignore
-6. add authentication module `auth.rb` in `infrastructures` directory
-
---
+++ frontend/src/usage.ts
@@ -0,0 +1,123 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import {Hello} from "./components/Hello";
+// (() => {
+//   window.addEventListener("load", (e) => {
+//     ReactDOM.render(
+//       <React.StrictMode>
+//         <Hello></Hello>
+//       </React.StrictMode>
+//       , document.getElementById("root")
+//     );
+//   });
+// })();
+import "./modal";
+import "./registerButton";
+import "./updateButton";
+import "./updateCheck";
+import "./bar.css";
+import "./deleteButton";
+import {setSelectedIndexText} from "./deleteButton";
+import {clearErrorMessage, setErrorMessage} from "./modal";
+
+type UPDATE_TYPE = "REGISTER" | "UPDATE"
+
+console.log("Hello, entry.ts");
+
+let selectedIndex = -1;
+export const getSelectedIndex = (): number => {
+  return selectedIndex;
+}
+window.addEventListener("load", (e) => {
+  console.log("getselected index");
+  const buttons = Array.from(document.querySelectorAll(".delete-button"));
+  buttons.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      const selected = <HTMLInputElement>(<HTMLInputElement>e.target)!.parentNode!.querySelector("#seq")
+      selectedIndex = Number(selected.value);
+      setSelectedIndexText(selectedIndex.toString());
+    });
+  });
+});
+
+export const getParam = () => {
+  const description = (<HTMLInputElement>document.getElementById("form_description")).value;
+  const form = <HTMLFormElement>document.getElementById("form_file");
+  return {
+    description,
+    form
+  };
+}
+
+export const getToken = () => {
+  const token = (<HTMLMetaElement>document.querySelector("head meta[name=\"csrf-token\"]")!).content;
+  return token;
+}
+
+export const submit = async (type: UPDATE_TYPE, token: string, description: string, form: Blob, index?: number) => {
+  clearErrorMessage();
+  const postData = new FormData();
+  console.log("submitUpdate");
+  postData.append("description", description);
+  if (index !== undefined) {
+    postData.append("selected_seq", index.toString());
+  }
+  if (form) {
+    postData.append("form_file", form);
+  }
+  const URL = type === "REGISTER" ? "/form/create" : "/form/update";
+  fetch(URL, {
+    headers: {
+      'X-CSRF-Token' : token
+    },
+    method: "POST",
+    body: postData
+  })
+  .then(async res => {
+    if (res.status === 200) {
+      location.reload();
+      return;
+    } else {
+      const resp = await res.json();
+      setErrorMessage(resp.message);
+    }
+  });
+}
+
+const validate = (form: HTMLFormElement) => {
+  console.log(form);
+  if (form.files.length === 0) {
+    return false;
+  }
+  return true;
+}
+export const toggleFormEnabled = (isUpdate: boolean) => {
+  const elements: HTMLInputElement[] = Array.from(document.querySelectorAll(".show-only-update-form"));
+  elements.forEach(element => {
+    element.disabled = !isUpdate
+  });
+}
+export const toggleStatus = (type: UPDATE_TYPE) => {
+  console.log("toggle");
+  const showOnlyRegisterElements = document.querySelectorAll(".show-only-register");
+  const showOnlyUpdateElements = Array.from(document.querySelectorAll(".show-only-update"));
+
+  showOnlyRegisterElements.forEach(showOnlyRegisterElement => {
+    showOnlyRegisterElement.classList.add("d-none");
+  });
+  showOnlyUpdateElements.forEach(showOnlyUpdateElement => {
+    showOnlyUpdateElement.classList.add("d-none");
+  });
+  if (type === "REGISTER") {
+    showOnlyRegisterElements.forEach(showOnlyRegisterElement => {
+      showOnlyRegisterElement.classList.remove("d-none");
+    });
+    const form = <HTMLFormElement>document.getElementById("form_file");
+    form.disabled = false;
+
+  } else {
+    showOnlyUpdateElements.forEach(showOnlyUpdateElement => {
+      showOnlyUpdateElement.classList.remove("d-none");
+    });
+  }
+}

374ebfb23fef83fef5ff91957e31d83a2d5fabe1
nkwtnb <n.watanabe0305@gmail.com>
2022-10-16 23:21:17 +0900

    メッセージ日本語化

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module ExcelCreator
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.use ActionDispatch::Cookies
+    config.i18n.default_locale = :ja
   end
 end

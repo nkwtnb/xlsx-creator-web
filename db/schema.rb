0cf11798e677099b7be013f90d1da6ae1ae4d85a
nkwtnb <n.watanabe0305@gmail.com>
2022-10-18 11:32:43 +0900

    userテーブル構成変更

--- db/schema.rb
+++ db/schema.rb
@@ -23,11 +23,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_095008) do
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "forms", "users"

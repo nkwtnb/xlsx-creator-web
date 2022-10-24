38f16c014433f4ae1ba78d513c588849777ccb4a
nkwtnb <n.watanabe0305@gmail.com>
2022-10-24 09:34:36 +0900

    jarの呼び出し方法を変更
    [`]の使用から[Open3.capture3]を使用する形へ変更
    標準出力、エラー内容、ステータスコードが取得可能となる

--- app/controllers/api/v1/form_controller.rb
+++ app/controllers/api/v1/form_controller.rb
@@ -1,6 +1,12 @@
+require "open3"
 module Api
   module V1
     class FormController < ActionController::API
+      STATUS = {
+        NORMAL_END: 0,
+        ABNORAMAL_END: 1,
+        TIMEOUT: 2
+      }
       include Auth
       def new
         user = get_authenticated_user_from_api(params)
@@ -14,9 +20,15 @@ module Api
           return render json: {message: "指定の帳票ID: #{form_id} に対応する帳票が存在しません。"}, status: :bad_request
         end
         parsed_data = JSON.generate(data.to_json)
-        base64 = `java -jar resources/xlsx-creator/xlsx-creator.jar "templates/#{form.file_name}" #{parsed_data}`
-        # TODO: エラーハンドリング
-        return render json: {base64: base64}, status: :ok
+        output, error, status = Open3.capture3("java -jar resources/xlsx-creator/xlsx-creator.jar \"templates/#{form.file_name}\" #{parsed_data}")
+
+        if status.exitstatus == STATUS[:NORMAL_END]
+          return render json: {base64: output}, status: :ok
+        elsif status.exitstatus == STATUS[:TIMEOUT]
+          return render json: {message: "帳票作成処理がタイムアウトしました"}, status: :request_timeout
+        else
+          return render json: {message: "帳票作成処理でエラーが発生しました"}, status: :bad_request
+        end
       end
 
       private

1f32711a7cf4689d1df0db6eed0529e89fca1639
nkwtnb <n.watanabe0305@gmail.com>
2022-10-25 21:58:43 +0900

    credential関連の事情により、GCS操作クラスをindex削除->再作成

---
+++ app/infrastructures/storage.rb
@@ -0,0 +1,31 @@
+require "google/cloud/storage"
+
+class Storage
+  def initialize
+    @storage = Google::Cloud::Storage.new(
+      project_id: ENV["RAILS_GCS_PROJECT"],
+      credentials: Rails.root.join("credentials.json").to_s
+    )
+    @bucket = @storage.bucket(ENV["RAILS_GCS_BUCKET"])
+  end
+  def get(file_name)
+    @bucket.file(file_name)
+  end
+  def save(contents, file_name)
+    begin
+      @bucket.create_file(StringIO.new(contents), file_name)
+    rescue => e
+      puts e.message
+      raise StandardError.new("ファイルの作成に失敗しました")
+    end
+  end
+  def delete(file_name)
+    file = get(file_name)
+    if file.nil?
+      raise StandardError.new("対象のファイルが存在しません")
+    end
+    if file.delete == false
+      raise StandardError.new("対象のファイルの削除に失敗しました")
+    end
+  end
+end

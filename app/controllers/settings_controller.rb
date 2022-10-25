068141e5d619decf4cd7544a518cd2d57bb09a1d
nkwtnb <n.watanabe0305@gmail.com>
2022-10-25 19:53:01 +0900

    アカウント削除時に当該ユーザーの登録テンプレートも削除する

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -23,9 +23,13 @@ class SettingsController < ApplicationController
   def delete
     begin
       user = get_authenticated_user
+      storage = Storage.new
+      forms = Form.where(user_id: user.id)
+      forms.each do |f|
+        storage.delete(f.file_name)
+      end
       user.destroy!
       cookies.delete :token
-      # TODO 登録した帳票テンプレートの削除
       flash[:success] = "アカウントを削除しました"
       return redirect_to sign_up_path
     rescue ActiveRecord::RecordInvalid => e

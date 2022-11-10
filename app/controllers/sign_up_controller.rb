4d58bdea967959776caaa4f24b796fbd561314f6
nkwtnb <n.watanabe0305@gmail.com>
2022-11-10 17:51:00 +0900

    エラーメッセージの表示をリストに変更

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -10,15 +10,18 @@ class SignUpController < ApplicationController
 
   def create
     begin
-      user = User.new(
+      @user = User.new(
         email: params[:email],
         password: params[:password],
         password_confirmation: params[:password_confirmation],
         )
-      user.save!
+      @user.save!
       sign_in(params[:email], params[:password])
+    rescue ActiveRecord::RecordInvalid
+      flash.now[:danger] = @user.errors.full_messages
+      render :new, status: :unprocessable_entity
     rescue => e
-      flash.now[:danger] = e.message
+      flash.now[:danger] = [e.message]
       render :new, status: :unprocessable_entity
     end
   end

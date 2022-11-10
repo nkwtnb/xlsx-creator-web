2d77136259aa8a71ea4eee72d558a4f01c3cee5f
nkwtnb <n.watanabe0305@gmail.com>
2022-11-10 12:42:20 +0900

    パスワードリセットメールの文面修正

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -4,12 +4,6 @@ class PasswordMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
-    mail(to: @user.email, subject: "Your password has been reset")
-    # @user = {
-    #   email: "n.watanabe0305@gmail.com",
-    #   name: "Naoki",
-    # }
-    # @url = "http://localhost:3000"
-    # mail(to: @user[:email], subject: '私の素敵なサイトへようこそ')
+    mail(to: @user.email, subject: "パスワードリセットのお知らせ")
   end
 end

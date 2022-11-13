42bf0af60fa17583aa1b7ced6ad772645f0adda3
nkwtnb <n.watanabe0305@gmail.com>
2022-11-13 22:48:42 +0900

    termsの内容はローカルから読み込めば良いので、そのように修正

--- app/helpers/terms_helper.rb
+++ app/helpers/terms_helper.rb
@@ -1,12 +1,7 @@
 module TermsHelper
   def get_terms
-    puts "hello Term"
-    uri = URI.parse("https://raw.githubusercontent.com/nkwtnb/xlsx-creator-web/main/TERMS.md")
-    response = Net::HTTP.get_response(uri)
-
-    puts response.code # status code
-    puts response.body.force_encoding("UTF-8") # response body
-    parsed = CommonMarker.render_html(response.body, :DEFAULT)
+    file = File.open(Rails.root.join("TERMS.md"))
+    parsed = CommonMarker.render_html(file.read, :DEFAULT)
     raw(parsed)
   end
 end

4edfc694df31553cb0e10b24160249fa48722736
nkwtnb <n.watanabe0305@gmail.com>
2023-05-17 02:58:27 +0900

    PDF化対応
    unoconvを使用し、xlsx->PDFの変換に対応
    それに伴い元Excelファイルをxlsx-creator.jar側で削除せず、Rails側で削除するように変更
    リクエスト時にpdfフラグを設定することでPDF変換有無を制御する

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM ruby:3.1.2
 
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+RUN apt-get update -qq \
+    && apt-get install -y build-essential libpq-dev nodejs unoconv  fonts-ipafont
 RUN mkdir /myapp
 WORKDIR /myapp
 ADD Gemfile /myapp/Gemfile
--- app/controllers/api/v1/form_controller.rb
+++ app/controllers/api/v1/form_controller.rb
@@ -1,4 +1,6 @@
 require "open3"
+require 'base64'
+
 module Api
   module V1
     class FormController < ActionController::API
@@ -24,33 +26,62 @@ module Api
         contents = file.download
         contents.rewind
         temp_file_name = SecureRandom.uuid + ".xlsx"
-        temp_file_full_path = Rails.root.join("resources", "xlsx-creator", "templates", temp_file_name)
+        temp_file_full_path = create_temp_path(temp_file_name)
         f = File.open(temp_file_full_path, "w+b")
         f.write(contents.read)
         f.close
-
         parsed_data = JSON.generate(data.to_json)
-        output, error, status = Open3.capture3("java -jar resources/xlsx-creator/xlsx-creator.jar \"templates/#{temp_file_name}\" #{parsed_data}")
-
-        if File.exist?(temp_file_full_path)
-          begin
-            File.delete(temp_file_full_path)
-          rescue => e
-            return render json: {message: "帳票作成の後処理でエラーが発生しました"}, status: :bad_request
-          end
-        end
-        if status.exitstatus == STATUS[:NORMAL_END]
-          return render json: {base64: output}, status: :ok
-        elsif status.exitstatus == STATUS[:TIMEOUT]
+        # Excel作成
+        excel_base64, e, s = create_excel(temp_file_name, parsed_data)
+        if s.exitstatus == STATUS[:TIMEOUT]
           return render json: {message: "帳票作成処理がタイムアウトしました"}, status: :request_timeout
-        else
+        elsif s.exitstatus != STATUS[:NORMAL_END]
           return render json: {message: "帳票作成処理でエラーが発生しました"}, status: :bad_request
         end
+        # Excel形式の場合はここで終了
+        body = JSON.parse(request.body.read)
+        if ActiveRecord::Type::Boolean.new.cast(body["pdf"]) == false
+          return render json: {base64: excel_base64}, status: :ok
+        end
+        # PDF作成
+        xlsx_file_path = create_temp_path(temp_file_name + "_temp.xlsx")
+        pdf_file_path = create_temp_path(temp_file_name + "_temp.pdf")
+        pdf_base64 = create_pdf(xlsx_file_path, pdf_file_path)
+        delete_files([temp_file_full_path, xlsx_file_path, pdf_file_path])
+        return render json: {base64: pdf_base64}, status: :ok
       end
 
       private
       # https://github.com/NoamB/sorcery/issues/724
       def form_authenticity_token; end
+      def create_excel(temp_file_name, parsed_data)
+        o, e, s = Open3.capture3("java -jar resources/xlsx-creator/xlsx-creator.jar \"templates/#{temp_file_name}\" #{parsed_data} 0")
+        return o, e, s
+      end
+      def create_pdf(xlsx_file_path, pdf_file_path)
+        result = ""
+        if File.exist?(xlsx_file_path)
+          o, e, s = Open3.capture3("unoconv -f pdf #{xlsx_file_path}")
+          if s.exitstatus == 0
+            # Excelファイルをバイナリモードで読み込む
+            excel_data = File.binread(pdf_file_path)
+            # ExcelデータをBase64エンコードする
+            result = Base64.strict_encode64(excel_data)
+          end
+        end
+        return result
+      end
+      def create_temp_path(temp_file_name)
+        Rails.root.join("resources", "xlsx-creator", "templates", temp_file_name)
+      end
+
+      def delete_files(files)
+        files.each do |file_path|
+          if File.exist?(file_path)
+            File.delete(file_path)
+          end
+        end
+      end
     end
   end
 end
